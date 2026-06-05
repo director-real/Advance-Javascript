fe9d7d5ff05b93edc27095e80c9e8a704d7ffe4e
DevIgnite-real <227312215+StackShadow-tech@users.noreply.github.com>
2026-06-05 19:01:27 -0400

    challenge completed about switches

---
+++ 05-Switch-Statements/index.js
@@ -0,0 +1,38 @@
+/*
+Price List
+Coffee $2
+Sandwiches $5
+Salad $4
+Lemon Cake $3
+ */
+
+// For example: "You selected Salad. That will be $4"
+
+function selectItem(item) {
+    let price = 0
+
+/*
+Challenge:
+1. Add the remaining price list items as cases.
+*/
+    switch(item) {
+        // item === 'Coffee'
+        case 'Coffee':
+            price = 2
+            break
+        case 'Sandwiches':
+            price = 5
+            break
+        case 'Salad':
+            price = 4
+            break
+        case 'Lemon Cake':
+            price = 3
+            break
+        default:
+            return `Sorry, we don't sell ${item}`
+    }   
+    return `You selected ${item}. That will be $${price}`
+}
+
+console.log(selectItem('pizza '))
