09a2ad01a36e5b5b486d57a7f562c3b3065645b0
DevIgnite-real <227312215+StackShadow-tech@users.noreply.github.com>
2026-06-05 14:04:42 -0400

    learend bit more about complex ternary operator

--- 03-Complex-Ternary-Operator/index.js
+++ 03-Complex-Ternary-Operator/index.js
@@ -1,17 +1,8 @@
 const exerciseTimeMins = 40
 
-let message = ''
 
-if (exerciseTimeMins < 30) {
-    message = 'You need to try harder!'
-}
-else if(exerciseTimeMins < 60) {
-    message = 'Doing good!'
-}
-else {
-    message = "Excellent!"
-}
-
-// const message = exerciseTimeMins < 30 ? 'You need to try harder!' : 'Doing good!
+const message = exerciseTimeMins < 30 ? 'You need to try harder!' 
+    : exerciseTimeMins < 60 ? 'Doing good!' 
+    : 'Excellent!'
 
 console.log(message)
