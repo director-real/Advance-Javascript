5207643e6f657916934b1518ab08ab5cecbc7f15
DevIgnite-real <227312215+StackShadow-tech@users.noreply.github.com>
2026-06-05 21:05:34 -0400

    learn what object-destructuring is

---
+++ 06-Object-Destructuring/index.js
@@ -0,0 +1,17 @@
+const favouriteFile = {
+    title: "Top Gun",
+    year: "1986",
+    genre: "action",
+    star: "Tom Cruise",
+    director: "Tony Scott"
+}
+
+const {title, year, genre, star, director} = favouriteFile
+
+// const title = favouriteFile.title
+// const year = favouriteFile.year
+// const genre = favouriteFile.genre
+// const star = favouriteFile.star
+// const director = favouriteFile.director
+
+console.log(`My favourite film is ${title} starring ${star}. It's an ${genre} film that was directed by ${director} and released in ${year}.`)
