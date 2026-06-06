b993e0657f01940f64080d2408b33b97ef205c85
DevIgnite-real <227312215+StackShadow-tech@users.noreply.github.com>
2026-06-05 21:16:32 -0400

    challenge completed

---
+++ 07-Object-Destructuring-challenge/06-Object-Destructuring/index.js
@@ -0,0 +1,10 @@
+const dreamHoliday = {
+    destination: 'Tokyo',
+    activity: 'TeamLab Planets',
+    accommodation: '5 star hotel',
+    companion: 'Mom & Grandma'
+}
+
+const {destination, activity, accommodation, companion} = dreamHoliday
+
+console.log(`I would love to go to ${destination}, to visit the ${activity}. I would like to stay at a ${accommodation} and hang out with my ${companion} all day.`)
