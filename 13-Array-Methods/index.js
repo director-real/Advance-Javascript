7194928f1e9c3ac9cd6b29dc1e0c64e97718e10a
director <227312215+StackShadow-tech@users.noreply.github.com>
2026-06-08 16:18:55 -0400

    learn modern way to right functions first

---
+++ 13-Array-Methods/index.js
@@ -0,0 +1,48 @@
+// Old way to write function
+// function double(number) {
+//     return number * 2
+// }
+
+
+// const double = (number) => {
+//     return number * 2
+// }
+
+// console.log(double(5))
+
+
+// const celebrateUser = (name, age) => {
+//     const upperName = name.toUpperCase()
+//     const userAge = age
+
+//     return `Woohoo! Amazing job, ${upperName} and your ${userAge} years old!`
+// }
+
+// console.log(celebrateUser("director-real", 20))
+
+
+// const triple = (num) => num * 20
+
+// console.log(triple(10))
+
+// const sayHello = () => "Hello, welcome back to the course!"
+
+// console.log(sayHello())
+
+// const triple = (num) => num * 20
+
+// console.log(triple(10))
+
+
+// const square = (num1) => num1 * num1
+
+// console.log(square(4))
+
+
+const makeEmail = (username) => {
+    const makeEmail = username + "@gmail.com"
+
+    return makeEmail
+}
+
+console.log(makeEmail("Myth"))
